22aa053ff20cce9fa330e955882af15b715a0645
MainInTheCloud <1256182832@qq.com>
2018-06-28 09:27:48 +0800

    fix problem of register

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -4,26 +4,26 @@ $(document).ready(function () {
     $.ajax(
         {
             type: "get",
-            url: "/api/machine/controler/time/latest/",
+            url: "http://106.15.230.111/api/machine/controler/time/latest/",
             dataType: "json",
             success: function (msg) {       //msg为返回的json数据
-                $.each(msg.data.list, function (index, sport) {
+                $.each(msg.data, function (index, sport) {
                     $(".water_in").html(sport.water_in);
                     $(".water_out").html(sport.water_out);
                     $(".COD").html(sport.COD);
                     $(".BOD").html(sport.BOD);
-                })
+                });
             }
         }
-    )
+    );
 
     $.ajax(
         {
             type: "get",
-            url: "/api/machine/processor/time/latest/",
+            url: "http://106.15.230.111/api/machine/processor/latest/",
             dataType: "json",
             success: function (msg) {
-                 $.each(msg.data.list, function (index, sport) {
+                 $.each(msg.data, function (index, sport) {
                     $(".level").html(sport.level);
                     $(".temperature").html(sport.temperature);
                     $(".PH").html(sport.PH);
@@ -31,5 +31,5 @@ $(document).ready(function () {
 
                 }）
         }
-    )
+    );
 });
