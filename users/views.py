e28d04524ba1f51d28e71bd596bc5e74f8c32df2
MainInTheCloud <1256182832@qq.com>
2018-06-28 10:53:10 +0800

    ...

--- users/views.py
+++ users/views.py
@@ -7,21 +7,14 @@ from .models import User
 from .utils import send_auth_email
 
 import logging
-logging.basicConfig(format='%(asctime)s:%(levelname)s:\n%(message)s',level=logging.INFO)
+
 
 class RegisterView(View):
-    
     def get(self, request):
         empty_form = RegisterForm()
         return render(request, 'registration/registration.html', context={'form': empty_form})
-    
-    def post(self, request):
-        logger = logging.getLogger('users.register')
 
-        file_handler = logging.FileHandler('/var/www/factory_monitor/log/register.log', mode='w')
-        stream_handler = logging.StreamHandler()
-        logger.addHandler(file_handler)
-        logger.addHandler(stream_handler)
+    def post(self, request):
         # request.POST 是一个类字典数据结构，记录了用户提交的注册信息
         # 这里提交的就是用户名（username）、密码（password）、确认密码、邮箱（email）
         # 用这些数据实例化一个用户注册表单
@@ -32,7 +25,7 @@ class RegisterView(View):
             user = User()
             user_name = request.POST.get("username", '')
             user_email = request.POST.get("email", '')
-            logger.info(form.clean_password2())
+
             user.username = user_name
             user.email = user_email
             user.password = make_password(form.clean_password2())
@@ -41,15 +34,16 @@ class RegisterView(View):
             user.save()
 
             # logging
+            logger = logging.getLogger('users.register')
             logger.info('New User {0} registered.'.format(user_name))
-            
-            
+            logger.info(form.clean_password2())
+            logger.info(user.password)
             # 验证邮箱
             send_auth_email(user_name, user_email)
             return redirect('/')
         else:
             return render(request, 'registration/registration.html', context={'form': form})
-         
+
 
 class ActiveUserView(View):
     def get(self, request, user_name, activate_code):
