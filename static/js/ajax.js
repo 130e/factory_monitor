1afaf03840bb9e9107f1e92f9ac222726b9ebaa2
MainInTheCloud <1256182832@qq.com>
2018-06-28 10:35:27 +0800

    ..

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,35 +1,37 @@
 $(document).ready(function () {
     setInterval("ask()",5000);
-    function ask()
-    $.ajax(
-        {
-            type: "get",
-            url: "http://106.15.230.111/api/machine/controler/time/latest/",
-            dataType: "json",
-            success: function (msg) {       //msg为返回的json数据
-                $.each(msg, function (index, sport) {
-                    $(".water_in").html(sport.water_in);
-                    $(".water_out").html(sport.water_out);
-                    $(".COD").html(sport.COD);
-                    $(".BOD").html(sport.BOD);
-                });
+    function ask(){
+        $.ajax(
+            {
+                type: "get",
+                url: "http://106.15.230.111/api/machine/controler/time/latest/",
+                dataType: "json",
+                success: function (msg) {       //msg为返回的json数据
+                    // msg = JSON.parse(msg);
+                    $.each(msg, function (index, sport) {
+                        $(".water_in").html(sport.water_in);
+                        $(".water_out").html(sport.water_out);
+                        $(".COD").html(sport.COD);
+                        $(".BOD").html(sport.BOD);
+                    });
+                }
             }
-        }
-    );
+        );
 
-    $.ajax(
-        {
-            type: "get",
-            url: "http://106.15.230.111/api/machine/processor/latest/",
-            dataType: "json",
-            success: function (msg) {
-                 $.each(msg, function (index, sport) {
-                    $(".level").html(sport.level);
-                    $(".temperature").html(sport.temperature);
-                    $(".PH").html(sport.PH);
-                    $(".density").html(sport.density);
+            // $.ajax(
+            //     {
+            //         type: "get",
+            //         url: "http://106.15.230.111/api/machine/processor/latest/",
+            //         dataType: "json",
+            //         success: function (msg) {
+            //              $.each(msg.data, function (index, sport) {
+            //                 $(".level").html(sport.level);
+            //                 $(".temperature").html(sport.temperature);
+            //                 $(".PH").html(sport.PH);
+            //                 $(".density").html(sport.density);
 
-                })
-        }
-    );
-});
+            //             }）
+            //     }
+            // );
+    }
+}
